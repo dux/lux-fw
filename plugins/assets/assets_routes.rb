eecdfa31b6c969af2de5c814281b8c63408c8eb8
Dino Reic <rejotl@gmail.com>
2018-09-16 18:03:52 +0200

    load assets routes only if we need them

--- plugins/assets/assets_routes.rb
+++ plugins/assets/assets_routes.rb
@@ -1,35 +1,35 @@
 # plug :local_assets
 # /compiled_asset/www/js/pjax.coffee
 # /raw_asset/www/js/pjax.coffee
-Lux.app.before do
-  next unless Lux.config(:compile_assets)
+if Lux.config(:compile_assets)
+  Lux.app.before do
+    # skip /favicon.ico that forces refresh
+    next unless nav.path[1]
 
-  # skip /favicon.ico that forces refresh
-  next unless nav.path[1]
-
-  # only allow clear in dev
-  # clear assets every 4 seconds max
-  if Lux.current.no_cache?
-    Lux.cache.fetch('lux-clear-assets', ttl: 4, log: false, force: false) do
-      puts '* Clearing assets from ./tmp/assets'.yellow
-      `rm -rf ./tmp/assets && mkdir ./tmp/assets`
-      true
+    # only allow clear in dev
+    # clear assets every 4 seconds max
+    if Lux.current.no_cache?
+      Lux.cache.fetch('lux-clear-assets', ttl: 4, log: false, force: false) do
+        puts '* Clearing assets from ./tmp/assets'.yellow
+        `rm -rf ./tmp/assets && mkdir ./tmp/assets`
+        true
+      end
     end
-  end
 
-  path = nav.rest.join('/')
+    path = nav.rest.join('/')
 
-  if nav.root == 'compiled_asset'
-    # allow plugin: paths
+    if nav.root == 'compiled_asset'
+      # allow plugin: paths
 
-    asset = SimpleAssets::Asset.new(path)
-    current.response.content_type asset.content_type
-    current.response.body asset.compile
+      asset = SimpleAssets::Asset.new(path)
+      current.response.content_type asset.content_type
+      current.response.body asset.compile
 
-  elsif nav.root == 'raw_asset'
-    Lux.error "You can watch raw files only in development" unless Lux.dev?
+    elsif nav.root == 'raw_asset'
+      Lux.error "You can watch raw files only in development" unless Lux.dev?
 
-    file = Lux.root.join('app/assets/%s' % path)
-    body file.exist? ? file.read : "error: File not found"
+      file = Lux.root.join('app/assets/%s' % path)
+      body file.exist? ? file.read : "error: File not found"
+    end
   end
 end
