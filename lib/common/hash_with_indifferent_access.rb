a1a0fbc37a1e8a45bbdb37a7d16e88c16d33178e
Dino Reic <rejotl@gmail.com>
2019-01-20 16:15:46 +0100

    master: simplified and optimized HashWithIndifferentAccess

--- lib/common/hash_with_indifferent_access.rb
+++ lib/common/hash_with_indifferent_access.rb
@@ -1,74 +1,42 @@
-class HashWithIndifferentAccess
-  def initialize data=nil
-    @data = {}
-    merge! data if data
-  end
+class HashWithIndifferentAccess < Hash
+  def initialize data={}
+    unless data.class == Hash
+      raise ArgumentError.new('Expected "Hash", not "%s"' % data.class)
+    end
 
-  def merge! data
-    data.each { |key, value| @data[convert_key(key)] = convert_value(value) }
-  end
-  alias_method :update, :merge!
-
-  def merge data
-    copy = self.class.new @data
-    copy.merge! data
-    copy
+    data.each do |key, value|
+      self[key.to_s] = value
+    end
   end
 
   def [] key
-    @data[convert_key(key)]
+    out = super key.to_s
+    out.class == Hash ? self.class.new(out) : out
   end
 
   def []= key, value
-    @data[convert_key(key)] = convert_value(value)
+    super key.to_s, value
   end
-  alias_method :store, :[]=
 
-  def key? name
-    @data.key? convert_key(name)
+  def key? key
+    super key.to_s
   end
   alias_method :include?, :key?
   alias_method :has_key?, :key?
   alias_method :member?, :key?
 
-  def to_json opts=nil
-    @data.to_json opts
-  end
-
-  def delete_if &block
-    @data.delete_if(&block)
-  end
-
   def delete key
-    @data.delete convert_key(key)
+    super key.to_s
   end
 
   def dig *args
-    list = args.map{ |it| it.is_a?(Symbol) ? it.to_s : it }
-    @data.dig *list
+    out = self
+
+    while key = args.shift
+      out = out[key]
+    end
+
+    out
   end
 
-  def pluck *args
-    args = args.map(&:to_s)
-    @data.select { |k,v| args.include?(k) }
-  end
-
-  def clear
-    @data.keys.each { |key| @data.delete(key) }
-  end
-
-  def each;    @data.each { |k,v| yield(k,v) }; end
-  def keys;    @data.keys; end
-  def values;  @data.keys; end
-  def to_hash; @data; end
-
-  private
-
-  def convert_key key
-    key.kind_of?(Symbol) ? key.to_s : key
-  end
-
-  def convert_value value
-    value.is_a?(Hash) ? self.class.new(value) : value
-  end
 end
--- spec/lib/hash_spec.rb
+++ spec/lib/hash_spec.rb
@@ -5,4 +5,28 @@ describe Hash do
     tag = { a:'123', b:'456' }.tag(:div, 'abc')
     expect(tag).to eq(%[<div a="123" b="456">abc</div>])
   end
+
+  it 'should create a hash with indifferent access' do
+    test = HashWithIndifferentAccess.new({
+      a: 1,
+      'b' => { 'c': { d:2 } }
+    })
+
+    expect(test[:a]).to eq(1)
+    expect(test['a']).to eq(1)
+
+    expect(test.key?(:a)).to eq(true)
+    expect(test.key?('a')).to eq(true)
+    expect(test.key?(:c)).to eq(false)
+    expect(test.key?('c')).to eq(false)
+
+    expect(test['b']['c']['d']).to eq(2)
+    expect(test[:b][:c][:d]).to eq(2)
+
+    expect(test.dig('b', 'c', :d)).to eq(2)
+    expect(test.dig(:b, :c, 'd')).to eq(2)
+
+    test.delete 'a'
+    expect(test[:a]).to eq(nil)
+  end
 end
