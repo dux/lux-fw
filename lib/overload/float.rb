8557fc82cb86901b3731648c2a843fe71a793c37
Dino Reic <rejotl@gmail.com>
2019-02-25 13:31:39 +0100

    more options for as_currency

--- lib/overload/float.rb
+++ lib/overload/float.rb
@@ -1,9 +1,19 @@
 class Float
 
-  def as_currency
+  def as_currency symbol=nil
     out = '%.2f' % self
     out = out.sub('.', ',')
-    out = out.sub(/(\d)(\d{3})/, '\1.\2')
+    out = out.sub(/(\d)(\d{3}),/, '\1.\2,')
+    out = out.sub(/(\d)(\d{3})\./, '\1.\2.')
+
+    if symbol
+      if symbol == '$'
+        out = '%s%s' % [symbol, out]
+      else
+        out += " #{symbol}"
+      end
+    end
+
     out
   end
 
