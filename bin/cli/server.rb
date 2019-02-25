231eb213c5ef9616f9bf3df6050df9ca5a706423
Dino Reic <rejotl@gmail.com>
2019-02-25 13:28:58 +0100

    added clean exit from puma server

--- bin/cli/server.rb
+++ bin/cli/server.rb
@@ -12,6 +12,8 @@ LuxCli.class_eval do
   method_option :env,   aliases: '-e', default: 'd',   desc: 'Environemnt, only first chart counts'
   method_option :rerun, aliases: '-r', default: false, desc: 'rerun app on every file chenge', type: :boolean
   def server
+    trap("SIGINT") { Cli.die 'ctrl+c exit' }
+
     mode  = 'LUX_MODE=%s' % options[:mode]
     env   = options[:env][0,1] == 'p' ? 'production' : 'development'
 
